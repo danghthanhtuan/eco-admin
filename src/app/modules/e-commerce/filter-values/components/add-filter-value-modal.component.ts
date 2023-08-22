8e23450e33e15f73522d02cb222fdc4de664c95a
tuandt2 <tuandt2@sendo.vn>
2023-08-22 22:42:32 +0700

    up

--- src/app/modules/e-commerce/filter-values/components/add-filter-value-modal.component.ts
+++ src/app/modules/e-commerce/filter-values/components/add-filter-value-modal.component.ts
@@ -41,21 +41,23 @@ export class AddFilterValueModalComponent implements OnInit, OnDestroy {
   };
   isLoading = false;
   subscriptions: Subscription[] = [];
-
+  typeSearchKey = 0;
   constructor(private filterValueService:  FilterValueService, public modal: NgbActiveModal,
     private fb: FormBuilder,private srvAlter: SwalService) { }
    
   ngOnInit(): void {
-    
     this.loadAttributeValue();
   }
 
   loadForm() {
     this.formGroup = this.fb.group({
-      // attributeValueName: [this.attributeValue.name,
-      //    Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(50)])],
-      //    url: [this.attributeValue.url,  Validators.compose([])],
-      //    attributeId: [this.attributeValue.attributeId,  Validators.compose([Validators.required])]
+      filterValueDisplayText: [this.filterValue.filterValueDisplayText,
+          Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(50)])],
+      filterSearchKey: [this.filterValue.filterSearchKey,  Validators.compose([])],
+      filterId: [this.filterValue.filterId,  Validators.compose([Validators.required])],
+      minPrice: [this.filterValue.minPrice, ],
+      maxPrice: [this.filterValue.maxPrice,  ],
+      sortOrder: [this.filterValue.sortOrder]
     });
 
     this.isLoading = true;
@@ -80,36 +82,40 @@ export class AddFilterValueModalComponent implements OnInit, OnDestroy {
 
   AddFilterValue() {
     this.isLoading = true;
-    // var modelPost : any = {
-    //   attributeID : this.attributeValue.attributeId,
-    //   attributeValuesRequest : [{
-    //     attributeValueName : this.attributeValue.name,
-    //     url :this.attributeValue.url,
-    //   }]
+    var modelPost : any = {
+      filterID : this.filterValue.filterId,
+      createFilterValueItems : [{
+        filterSearchKey : this.filterValue.filterSearchKey,
+        filterValueDisplayText :this.filterValue.filterValueDisplayText,
+        sortOrder :this.filterValue.sortOrder,
+        minPrice :this.filterValue.minPrice,
+        maxPrice :this.filterValue.maxPrice
+      }]
 
-    // }
-    // const sb = this.filterValueService.create(modelPost, '/v1/filter/value').pipe(
-    //   delay(500), // Remove it from your code (just for showing loading)
-    //   tap(() =>
-    //   //  this.modal.close() thêm thành công thêm nữa ko close
-    //   {}
-    //   ),
-    //   catchError((err) => {
-    //     this.modal.dismiss(err);
-    //     this.srvAlter.toast(TYPE.ERROR, "Thêm không thành công!", false);
-    //     return of(undefined);
-    //   }),
-    //   finalize(() => {
-    //     //this.isLoading = false;
-    //     // tính sau 
-    //     this.srvAlter.toast(TYPE.SUCCESS, "Thêm thành công!", false);
-    //     this.filterValueService.fetch();
-    //   })
-    // ).subscribe(
+    }
+    debugger;
+    const sb = this.filterValueService.create(modelPost, '/v1/filters/value').pipe(
+      delay(500), // Remove it from your code (just for showing loading)
+      tap(() =>
+      //  this.modal.close() thêm thành công thêm nữa ko close
+      {}
+      ),
+      catchError((err) => {
+        this.modal.dismiss(err);
+        this.srvAlter.toast(TYPE.ERROR, "Thêm không thành công!", false);
+        return of(undefined);
+      }),
+      finalize(() => {
+        //this.isLoading = false;
+        // tính sau 
+        this.srvAlter.toast(TYPE.SUCCESS, "Thêm thành công!", false);
+        this.filterValueService.fetch();
+      })
+    ).subscribe(
      
       
-    //   );
-    // this.subscriptions.push(sb);
+      );
+    this.subscriptions.push(sb);
   }
 
   edit() {
@@ -160,8 +166,19 @@ export class AddFilterValueModalComponent implements OnInit, OnDestroy {
     this.subscriptions.forEach(sb => sb.unsubscribe());
   }
 
+  onChange(value : string){
+    var item = this.listFilters.filter(
+      f => f.id == value);
+
+      debugger;
+    if(item)
+    {
+      this.typeSearchKey = item[0].typeSearch;
+    }  
+  }
+
   save() {
-  
+  debugger;
     this.prepareAttribute();
     if (!this.formGroup.valid) {
       this.validateAllFormFields(this.formGroup);
@@ -177,8 +194,13 @@ export class AddFilterValueModalComponent implements OnInit, OnDestroy {
 
   private prepareAttribute() {
     const formData = this.formGroup.value;
-    // this.attributeValue.name = formData.attributeValueName;
-    // this.attributeValue.url = formData.url;
+     this.filterValue.sortOrder = formData.sortOrder;
+     this.filterValue.filterValueDisplayText = formData.filterValueDisplayText;
+     this.filterValue.filterId = formData.filterId; 
+     this.filterValue.minPrice = formData.minPrice;
+     this.filterValue.filterSearchKey = formData.filterSearchKey;
+     this.filterValue.maxPrice = formData.maxPrice;
+    
   }
 
   // helpers for View
